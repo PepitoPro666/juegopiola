4a82514acd490034016b2d415d076003a53cf1c6
PepitoPro666 <imrydark@gmail.com>
2025-05-24 06:10:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const gameServer = new Server({
 
 gameServer.define("game", GameRoom);
 
+app.get("/", (req, res) => {
+  res.send("Servidor Colyseus en funcionamiento");
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);  // <-- Cambiar gameServer.listen a server.listen
 console.log("Servidor corriendo en el puerto", PORT);
